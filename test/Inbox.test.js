35b03c74ffdbee78ff4ce43bd2008f6f51be03f2
shoto13 <wilsonrc@pm.me>
2022-08-06 21:43:59 +0100

    work on mocha testing

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,6 +4,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
+const INITIAL_STRING = 'init message';
 
 let accounts;
 let inbox;
@@ -21,7 +22,18 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
 
